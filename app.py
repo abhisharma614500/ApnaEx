c5600eb04c8d557d42828c4e86bddb6869abec17
abhisharma614500 <abhisharma614500@gmail.com>
2026-09-13 03:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+from flask import Flask, send_from_directory
 import os
-from flask import Flask
 
-app = Flask(__name__)
-PORT = int(os.environ.get("PORT", 8080))
+app = Flask(__name__, static_folder='.')
 
-@app.route("/")
-def home():
-    return "Hello, Render!"
+@app.route('/')
+def serve_index():
+    # यह Vercel को सीधा आपकी मुख्य HTML फ़ाइल पर भेज देगा
+    return send_from_directory('.', 'index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+if __name__ == '__main__':
+    app.run(debug=True)
